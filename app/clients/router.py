6cbc6ef0be10fa0200bfe46d2217fd46e58f59c7
Jiayi Zhou <zhou.jiayi4@northeastern.edu>
2025-04-04 13:25:46 -0700

    edit router

--- app/clients/router.py
+++ app/clients/router.py
@@ -33,6 +33,7 @@ async def predict(data: PredictionInput):
 async def get_clients(
         skip: int = Query(default=0, ge=0, description="Number of records to skip"),
         limit: int = Query(default=50, ge=1, le=150, description="Maximum number of records to return"),
+        _: User = Depends(get_current_user),
         db: Session = Depends(get_db)
 ):
     return ClientService.get_clients(db, skip, limit)
